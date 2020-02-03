30e40f1c0e5c9c1f63053bc9d24fd6fd6126b27e
Josh Eslinger <jbeslinger@gmail.com>
2020-02-03 15:33:14 -0600

    Added "vec2" 2D vector class with arithmetic methods & overloaded '<<' operator functionality.

--- Sparky-core/Sparky-core/main.cpp
+++ Sparky-core/Sparky-core/main.cpp
@@ -1,43 +1,23 @@
 #include "src/graphics/window.h"
+#include "src/math/vec2.h"
 #include <iostream>
 
 int main()
 {
 	using namespace sparky;
 	using namespace graphics;
+	using namespace math;
 
-	Window window("Hello Window!", 480, 270);
+	Window window("Sparky!", 480, 270);
 	glClearColor(0.5f, 0.0f, 1.0f, 1.0f);
 
-	// Dumb triangle variables
-	float xOrigin = 0.0f;
-	float yOrigin = 0.0f;
-	float speed = 0.001f;
+	vec2 myVector(1.0f, 2.0f);
 
+	std::cout << myVector << std::endl;
+	
 	while (!window.closed())
 	{
 		window.clear();
-
-		// Check if mouse button is being pressed
-		if (window.isMouseButtonPressed(GLFW_MOUSE_BUTTON_LEFT))
-			std::cout << "Left mouse button clicked!" << std::endl;
-
-		// Move the triangle
-		if (window.isKeyPressed(GLFW_KEY_RIGHT))
-			xOrigin += speed;
-		else if (window.isKeyPressed(GLFW_KEY_LEFT))
-			xOrigin -= speed;
-		else if (window.isKeyPressed(GLFW_KEY_UP))
-			yOrigin += speed;
-		else if (window.isKeyPressed(GLFW_KEY_DOWN))
-			yOrigin -= speed;
-
-		// Draw triangle
-		glBegin(GL_TRIANGLES);
-		glVertex2f(xOrigin-0.25f, yOrigin-0.25f);
-		glVertex2f(xOrigin+0.0f, yOrigin+0.25f);
-		glVertex2f(xOrigin+0.25f, yOrigin-0.25f);
-		glEnd();
 
 		window.update();
 	}
