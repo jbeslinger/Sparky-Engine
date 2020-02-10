6d2348ba72f13949091dbc0d9136453f98fb447b
Josh Eslinger <jbeslinger@gmail.com>
2020-02-10 14:29:42 -0600

    Added Layer superclass and TileLayer subclass.  Cleared out a lot of debug lines in main.cpp.

--- Sparky-core/Sparky-core/main.cpp
+++ Sparky-core/Sparky-core/main.cpp
@@ -30,43 +30,8 @@ int main()
 	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 
 	Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
-	shader.enable();
-	shader.setUniformMat4("pr_matrix", ortho);
-
-	std::vector<Renderable2D*> sprites;
-
-	srand(time(NULL));
-
-	for (float y = 0; y < 9.0f; y += 0.1f)
-	{
-		for (float x = 0; x < 16.0f; x += 0.1f)
-		{
-			sprites.push_back(new
-#if BATCH_RENDERER
-				Sprite
-#else
-				StaticSprite
-#endif
-				(x, y, 0.08f, 0.08f, math::vec4(rand() % 1000 / 1000.0f, 0, 1, 1)
-#if !BATCH_RENDERER				
-					, shader
-#endif
-					));
-		}
-	}
-
-#if BATCH_RENDERER
-	Sprite sprite1(5, 5, 4, 4, math::vec4(1, .6, 0, 1));
-	Sprite sprite2(7, 1, 2, 3, math::vec4(0.2f, 0, 1, 1));
-	BatchRenderer2D renderer;
-#else
-	StaticSprite sprite1(5, 5, 4, 4, math::vec4(1, 0, 1, 1), shader);
-	StaticSprite sprite2(7, 1, 2, 3, math::vec4(0.2f, 0, 1, 1), shader);
-	Simple2DRenderer renderer;
-#endif
 
 	shader.setUniform2f("light_pos", vec2(4.0f, 1.5));
-	shader.setUniform4f("light_col", vec4(0.2f, 0.3f, 0.8f, 1.0f));
 
 	Timer time;
 	float timer = 0.0f;
@@ -74,26 +39,13 @@ int main()
 
 	while (!window.closed())
 	{
-		mat4 mat = mat4::translation(vec3(5, 5, 5));
-		mat *=  mat4::rotation(time.elapsed() * 50.0f, vec3(0, 0, 1));
-		mat *=  mat4::translation(vec3(-5, -5, -5));
-		shader.setUniformMat4("ml_matrix", mat);
-
 		window.clear();
 		double x, y;
 		window.getMousePosition(x, y);
 		shader.setUniform2f("light_pos", vec2((float)(x * 16.0f / 960.0f), (float)(9.0f - y * 9.0f / 540.0f)));
-#if BATCH_RENDERER
-		renderer.begin();
-#endif
-		for (int i = 0; i < sprites.size(); i++)
-		{
-			renderer.submit(sprites[i]);
-		}
-#if BATCH_RENDERER
-		renderer.end();
-#endif
-		renderer.flush();
+
+
+
 		window.update();
 		frames++;
 		if (time.elapsed() - timer > 1.0f)
